94302a8b05c142ae07a4b269cfc7dce4288e2733
Jai Agrawal <jaijan59@gmail.com>
2017-08-20 00:05:03 +0530

    Add files via upload

---
+++ dashboard/connection.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(0);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database ="cms";
+$conn = mysqli_connect($servername,$username,$password,$database);
+?>
